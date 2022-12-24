442c18ee51e3c12ec583dfb346ace5ff0ff49b0e
kunal <kunalpatil2203@gmail.com>
2022-12-24 21:53:18 +0530

    Server side integration

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+require('dotenv').config();
 const { SerialPort, ReadlineParser } = require('serialport');
+const serialport = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 });
+const parser = serialport.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+const express = require('express');
+const http = require('http');
+const io = require('socket.io');
+const path = require('path');
 
-const serialport = new SerialPort({path: '/dev/ttyACM0', baudRate: 9600});
+// Constants
+const port = process.env.PORT || 3000;
 
-const parser = serialport.pipe(new ReadlineParser({delimiter: '\r\n'}))
+// HTTP and Web Socket server instances.
+const app = express();
+const httpServer = http.createServer(app);
+const ioServer = new io.Server(httpServer);
 
-parser.on('data', (chunk) => {
-    [value, unit] = chunk.split(" ");
-    const data = { value, unit };
-    console.log(data)
+// Serving index.html file for the client.
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
+
+// listner for client connections on socket.
+ioServer.on('connection', (socket) => {
+    // Serialport parser to parse data from buffer to text.
+    parser.on('data', (chunk) => {
+        const data = { value: chunk };
+        // emitting data to the client.
+        socket.emit('data', data);
+    })
+})
+
+// Server start
+httpServer.listen(port, console.log(`Listening on port: ${port}`));
